bafe5470360db9ef434ce59d30fe86410d2c0890
Jeffrey Stoke <me@arhat.dev>
2020-09-18 19:18:51 +0200

    Update ci test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,15 +1,15 @@
 import {run} from '../src/run'
 import {Result} from '../src/type'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {assert} from 'console'
 
-test('sh', async () => {
-  const ret = await run('sh', 'printf test')
-  const r = ret as Result
-  expect(r.stdout_data).toEqual('test')
-})
+test(
+  'sh',
+  async () => {
+    const ret = await run('sh', 'printf test')
+    const r = ret as Result
+    expect(r.stdout_data).toEqual('test')
+  },
+  100 * 1000
+)
 
 test('invalid shell', async () => {
   const ret = await run('foo', 'printf test')
